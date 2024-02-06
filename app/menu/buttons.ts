7b16c4ad47cb27187c2bfd06a7cdb89044851084
b <teslensky@gmail.com>
2024-02-06 14:02:45 +0200

    fix texts

--- app/menu/buttons.ts
+++ app/menu/buttons.ts
@@ -59,13 +59,15 @@ const getSettingsButtons = (state, language = 'en'): CallbackButton[][] => [
     [createButton('back', true, language), createButton('close', true, language)]
 ];
 
-const getHelpButtons = (language = 'en') => [
-    ['0', '1', '2', '3'].map(option => createButton(`helpOption:${option}`, true, language)),
-    [
-        createButton('back', true, language),
-        createButton('close', true, language)
-    ]
-];
+const getHelpButtons = (language: string = 'en'): CallbackButton[][] => {
+    return [
+        [Markup.callbackButton(`${language === 'ru' ? 'Что это вообще?' : 'Annotations'}`, 'helpOption:0')],
+        [Markup.callbackButton(`${language === 'ru' ? 'Список платформ' : 'Annotations'}`, 'helpOption:1')],
+        [Markup.callbackButton(`${language === 'ru' ? 'Бот в группе или канале' : 'Annotations'}`, 'helpOption:2')],
+        [Markup.callbackButton(`${language === 'ru' ? 'Аннотации' : 'Annotations'}`, 'helpOption:3')],
+        [Buttons.getBackButton(language), Buttons.getCloseButton(language)]
+    ];
+};
 
 const getLeaveSceneButton = (language = 'en'): CallbackButton[] => [createButton('cancel', true, language)];
 
