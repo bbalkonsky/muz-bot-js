6593262b59a9cdd47ffd3355913bfec147f8efcb
b <teslensky@gmail.com>
2025-01-01 19:58:06 +0200

    PIDR

--- app/helpers/info.ts
+++ app/helpers/info.ts
@@ -35,8 +35,8 @@ export default class Info {
             link: ['tidal.com']
         },
         vk: {
-            alias: 'VK',
-            link: ['vk.com']
+            alias: 'VKontakte',
+            link: ['vkontakte.ru']
         },
         // 'pandora': {alias: 'Pandora', link: 'pandora.com'},
         // 'napster': {alias: 'Napster', link: 'napster.com'},
